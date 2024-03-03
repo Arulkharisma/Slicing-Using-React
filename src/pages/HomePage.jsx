d55b395af7d5354ec8d1632acf877ea82fabcd55
Arulkharisma <arulkharisma49@gmail.com>
2024-03-04 06:52:00 +0700

    change structure folder

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap"
+import { BsArrowRight } from "react-icons/bs";
 
 const HomePage = () => {
   return (
@@ -8,7 +9,7 @@ const HomePage = () => {
           <div className="col-md-6 left-hero">
             <h1 className="title-hero">Search & review <br />your <span>fav book</span> <br />effortlessly</h1>
             <p className="description-hero mb-4 mt-4">Embark on a literary journey like never before with our revolutionary library application! Introducing a seamless experience that transcends traditional boundaries, where  you can effortlessly search your favorite books.✨</p>
-            <button>Start Now</button>
+            <button>Start Now <BsArrowRight/> </button>
           </div>
           <div className="col-md-6">
             <img src="/Left.png" alt="hero-image" />
