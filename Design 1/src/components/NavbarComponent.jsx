07aab89895d1436177d84fb3985c33ddb8af6ba1
Arulkharisma <arulkharisma49@gmail.com>
2024-03-04 04:26:12 +0700

    add hero section and styling navbar

--- Design 1/src/components/NavbarComponent.jsx
+++ Design 1/src/components/NavbarComponent.jsx
@@ -1,27 +1,53 @@
+import { useState, useEffect } from "react"
 import { Nav,Navbar, Container } from "react-bootstrap" 
 import { NavLink } from "react-router-dom"
 import { NavLinks } from "../data"
 
 const NavbarComponent = () => {
+  const [changeColor, setChangeColor] = useState(false);
+  const [changeText, setChangeText] = useState(false);
+
+  const changeBackground = () =>{
+    if(window.scrollY > 50){
+      setChangeColor(true)
+      setChangeText(true)
+    }else{
+      setChangeColor(false);
+      setChangeText(false);
+    }
+  };
+
+  useEffect(() =>{
+    changeBackground();
+
+    window.addEventListener("scroll", changeBackground)
+  })
+
+
+
   return (
     <div>
-     <Navbar expand="lg" className="transparent text-center">
+     <Navbar expand="lg" className={`transparent fixed-top text-center ${changeColor ? "bg-active" : ""}`}>
       <Container>
-        <Navbar.Brand className="me-4" href="/"><img src="/Logo.png" alt="logo" className="me-1" /> <span className="fw-bold fs-5 align-middle">Lidia</span></Navbar.Brand>
+        <Navbar.Brand className="me-4" href="/"><img src="/Logo.png" alt="logo" className="me-1" /> <span className={`fw-bold fs-5 align-middle ${changeText ? "color-active" : ""}`}>Arkha</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="ps-5 pe-5 justify-content-between">
           <Nav className="d-flex">
             {
               NavLinks.map((links) =>{
                 return(
-                  <div key={links.id} className="">
-                    <NavLink className="text-black link-style m-2" to={links.path}>{links.text}</NavLink>
+                  <div key={links.id} className={`link-style`}>
+                    <NavLink 
+                      to={links.path} 
+                      className={({ isActive, isPending }) =>isPending ? "pending" : isActive ? "active" : ""} end>
+                        {links.text}
+                    </NavLink>
                   </div>
                 )  
             })}
           </Nav>
           <div>
-            <button className="justify-content-end pb-2 pt-2 pe-4 ps-4 rounded bg-white fw-bold">Login</button>
+            <button className="justify-content-end pb-2 pt-2 pe-4 ps-4 rounded bg-white fw-bold border-0">Login</button>
           </div>
         </Navbar.Collapse>
       </Container>
--- Design 1/src/pages/HomePage.jsx
+++ Design 1/src/pages/HomePage.jsx
@@ -1,8 +1,25 @@
+import { Container } from "react-bootstrap"
 
 const HomePage = () => {
   return (
-    <div>
-        
+    <div className="bg-hero w-100 vh-100">
+      <Container>
+        <section className="row align-items-center vh-100">
+          <div className="col-md-6 left-hero">
+            <h1 className="title-hero">Search & review <br />your <span>fav book</span> <br />effortlessly</h1>
+            <p className="description-hero mb-4 mt-4">Embark on a literary journey like never before with our revolutionary library application! Introducing a seamless experience that transcends traditional boundaries, where  you can effortlessly search your favorite books.✨</p>
+            <button>Start Now</button>
+          </div>
+          <div className="col-md-6">
+            <img src="/Left.png" alt="hero-image" />
+          </div>
+        </section>
+      </Container>
+      <Container>
+        <section className="">
+          <h1>fdsffsdfdfsdfd</h1><br /><br /><br /><br /><br /><br /><br />
+        </section>
+      </Container>
     </div>
   )
 }
