b79bdcc04a0639a29a6f24d00316f138ca206e42
Arulkharisma <arulkharisma49@gmail.com>
2024-03-04 19:50:29 +0700

    add feature page

--- src/pages/FeaturePage.jsx
+++ src/pages/FeaturePage.jsx
@@ -1,7 +1,25 @@
+import { Col, Container, Row } from "react-bootstrap"
+import { features } from "../data"
 
 const FeaturePage = () => {
   return (
-    <div>FeaturePage</div>
+    <div>
+      <Container className="feature-page">
+        <p>FEATURES</p>
+        <h2 className="fw-bold">🤔. What You Can Do?</h2>
+        <Row>
+          {features.map((feature) =>{
+            return(
+              <Col key={feature.id} className="text-center p-2 feature-content">
+                <img src={feature.image} alt="icon-feature" width="250"/>
+                <h4 className="fw-bold">{feature.title}</h4>
+                <p className="pe-4 ps-4">{feature.description}</p>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+    </div>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,26 @@
 import { Container } from "react-bootstrap"
 import { BsArrowRight } from "react-icons/bs";
+import FeaturePage from "./FeaturePage";
 
 const HomePage = () => {
   return (
-    <div className="bg-hero w-100 vh-100">
-      <Container>
-        <section className="row align-items-center vh-100">
-          <div className="col-md-6 left-hero">
-            <h1 className="title-hero">Search & review <br />your <span>fav book</span> <br />effortlessly</h1>
-            <p className="description-hero mb-4 mt-4">Embark on a literary journey like never before with our revolutionary library application! Introducing a seamless experience that transcends traditional boundaries, where  you can effortlessly search your favorite books.✨</p>
-            <button>Start Now <BsArrowRight/> </button>
-          </div>
-          <div className="col-md-6">
-            <img className="image-hero" src="/Left.png" alt="hero-image" width="600px"/>
-          </div>
-        </section>
-      </Container>
-      <Container>
-        <section className="">
-          <h1>fdsffsdfdfsdfd</h1><br /><br /><br /><br /><br /><br /><br />
-        </section>
-      </Container>
-    </div>
+    <>
+      <div className="bg-hero">
+        <Container>
+          <section className="row section-hero w-100 vh-100 row align-items-center">
+            <div className="col-md-6 left-hero">
+              <h1 className="title-hero">Search & review <br />your <span>fav book</span> <br />effortlessly</h1>
+              <p className="description-hero mb-4 mt-4">Embark on a literary journey like never before with our revolutionary library application! Introducing a seamless experience that transcends traditional boundaries, where  you can effortlessly search your favorite books.✨</p>
+              <button>Start Now <BsArrowRight/> </button>
+            </div>
+            <div className="col-md-6">
+              <img className="image-hero" src="/Left.png" alt="hero-image" width="550px"/>
+            </div>
+          </section>
+        </Container>
+      </div>
+      <FeaturePage />
+    </>
   )
 }
 
