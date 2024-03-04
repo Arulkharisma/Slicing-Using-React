f19417816cc08979f846782ea31a5c625266c6f4
Arulkharisma <arulkharisma49@gmail.com>
2024-03-04 19:49:48 +0700

    add data for feature page

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,6 @@
+import reviewIcon from '../assets/img/reviewIcon.png'
+import wishlistIcon from '../assets/img/wishlistIcon.png'
+
 export const NavLinks = [
     {
         id:1,
@@ -25,4 +28,25 @@ export const NavLinks = [
         text:"Location"
     },
 
+]
+
+export const features = [
+    {
+        id: 1,
+        image: wishlistIcon,
+        title: "Search book",
+        description: "Effortlessly find your next read with our powerful and intuitive book search.",
+    },
+    {
+        id: 2,
+        image: reviewIcon,
+        title: "Review Book",
+        description: "Discover insightful critiques  and share your thoughts on diverse literary masterpieces effortlessly.",
+    },
+    {
+        id: 3,
+        image: wishlistIcon,
+        title: "Wishlist Book",
+        description: "Curate your literary dreams–wishlist books  for future adventures and discoveries.",
+    }
 ]
