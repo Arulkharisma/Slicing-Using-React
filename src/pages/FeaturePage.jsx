669f56353073f22ff00ea3a12c2dcfed0b07d64c
Arulkharisma <arulkharisma49@gmail.com>
2024-03-07 07:07:15 +0700

    edit style

--- src/pages/FeaturePage.jsx
+++ src/pages/FeaturePage.jsx
@@ -3,7 +3,7 @@ import { features } from "../data"
 
 const FeaturePage = () => {
   return (
-    <div>
+    <div className="pb-5">
       <Container className="feature-page">
         <p>FEATURES</p>
         <h2 className="fw-bold">🤔. What You Can Do?</h2>
@@ -11,7 +11,7 @@ const FeaturePage = () => {
           {features.map((feature) =>{
             return(
               <Col key={feature.id} className="text-center p-2 feature-content">
-                <img src={feature.image} alt="icon-feature" width="250"/>
+                <img src={feature.image} alt="icon-feature" width="200"/>
                 <h4 className="fw-bold">{feature.title}</h4>
                 <p className="pe-4 ps-4">{feature.description}</p>
               </Col>
